2064f6ebf1581ede07d2c7067a98fe6967193114
cfichtmueller <cfichtmueller@lokirobotics.de>
2015-08-04 10:45:27 +0200

    initial commit

---
+++ template-jenkins-wall-display.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env/python
+
+from wallDisplay import WallDisplay
+
+def exit_handler(signal, frame):
+	global display
+	display.close()
+	sys.exit(0)
+
+signal.signal(signal.SIGTERM, exit_handler)
+
+display = WallDisplay(%JENKINS_URL%, %JENKINS_USER%, %JENKINS_API_KEY%, %VIEW_ID%)
+
+display.show()
+
+while True:
+	continue
+	
